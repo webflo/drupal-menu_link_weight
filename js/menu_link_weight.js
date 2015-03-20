b69e7fca7d68fb281e6300a5dfc9316ac12beaf9
stefan.r <stefan.r@551886.no-reply.drupal.org>
2015-03-20 16:33:54 +0100

    Add real-time updating of link title

---
+++ js/menu_link_weight.js
@@ -0,0 +1,24 @@
+/**
+ * @file Menu Link Weight Javascript functionality.
+ */
+
+(function ($) {
+  /**
+   * Automatically update the current link title in the menu link weight list.
+   */
+  Drupal.behaviors.menuLinkSyncAutomaticTitle = {
+    attach: function (context) {
+      $('fieldset.menu-link-form', context).each(function () {
+        var $checkbox = $('.form-item-menu-enabled input', this);
+        var $link_title = $('.form-item-menu-link-title input', context);
+        var $current_selection = $('.menu-link-weight-link-current', context);
+
+        // Take over any link title change.
+        $link_title.keyup(function () {
+          $current_selection.html($link_title.val());
+        });
+      });
+    }
+  };
+
+})(jQuery);
