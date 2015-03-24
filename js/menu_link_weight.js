cb8c8816f52add301d6b40b0b7339df750d7217a
stefan.r <stefan.r@551886.no-reply.drupal.org>
2015-03-24 20:13:59 +0100

    If there is no title, take over the title of the link

--- js/menu_link_weight.js
+++ js/menu_link_weight.js
@@ -13,7 +13,10 @@
         var $checkbox = $('.form-item-menu-enabled input', this);
         var $link_title = $('.form-item-menu-link-title input', context);
         var $current_selection = $('.menu-link-weight-link-current', context);
-
+        // If there is no title, take over the title of the link.
+        if ($current_selection.html() == '') {
+          $current_selection.html($link_title.val());
+        }
         // Take over any link title change.
         $link_title.keyup(function () {
           $current_selection.html($link_title.val());
