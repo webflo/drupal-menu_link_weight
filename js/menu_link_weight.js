127a85ffbe56a30b52a1af90f96155422cb5e392
stefan.r <stefan.r@551886.no-reply.drupal.org>
2015-03-27 12:47:57 +0100

    Take over on node title change

--- js/menu_link_weight.js
+++ js/menu_link_weight.js
@@ -13,12 +13,16 @@
         var $checkbox = $('.form-item-menu-enabled input', this);
         var $link_title = $('.form-item-menu-link-title input', context);
         var $current_selection = $('.menu-link-weight-link-current', context);
+        var $node_title = $(this).closest('form').find('.form-item-title input');
         // If there is no title, take over the title of the link.
         if ($current_selection.html() == '') {
           $current_selection.html($link_title.val().substring(0, 30));
         }
         // Take over any link title change.
         $link_title.keyup(function () {
+          $current_selection.html($link_title.val().substring(0, 30));
+        });
+       $node_title.keyup(function () {
           $current_selection.html($link_title.val().substring(0, 30));
         });
       });
